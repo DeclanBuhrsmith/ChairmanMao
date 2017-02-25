24cd38efa8ace2b68f4113d488fa6a8ea4a9bdee
declanb <declanb@iastate.edu>
2017-02-24 23:41:04 -0600

    Ini

---
+++ wear/src/main/java/com/decdaddy/chairmanmao/AnalogWatchFaceService.java
@@ -0,0 +1,59 @@
+package com.decdaddy.chairmanmao;
+
+import android.os.Bundle;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+/**
+ * Created by declan on 2/24/2017.
+ */
+
+public class AnalogWatchFaceService extends CanvasWatchFaceService {
+
+    @Override
+    public Engine onCreateEngine() {
+        /* provide your watch face implementation */
+        return new Engine();
+    }
+
+    /* implement service callback methods */
+    private class Engine extends CanvasWatchFaceService.Engine {
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+            /* initialize your watch face */
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            super.onPropertiesChanged(properties);
+            /* get device features (burn-in, low-bit ambient) */
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            /* the time changed */
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            /* the wearable switched between modes */
+        }
+
+        //@Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            /* draw your watch face */
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            /* the watch face became visible or invisible */
+        }
+    }
+}
